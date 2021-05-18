4be0a10977ad8c18e91f1b950689de8e564362ca
Teegan <45438370+Sevrous@users.noreply.github.com>
2021-05-18 17:21:14 +0200

    Add files via upload

---
+++ app/src/main/java/com/whatsnextfood/whatsnexthomescreen/Contact.kt
@@ -0,0 +1,14 @@
+package com.whatsnextfood.whatsnexthomescreen
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class contact : AppCompatActivity(){
+
+    override fun onCreate (savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contact_pagina)
+        //comment
+    }
+}
