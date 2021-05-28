f89af9f780b206de8871f15dbcece28cc8511253
Charlotte-Cauberghe <r0831481@student.thomasmore.be>
2021-05-28 19:28:16 +0200

    kast listview

--- app/src/main/java/com/whatsnextfood/whatsnexthomescreen/DeKast.kt
+++ app/src/main/java/com/whatsnextfood/whatsnexthomescreen/DeKast.kt
@@ -1,9 +1,15 @@
 package com.whatsnextfood.whatsnexthomescreen
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 
 class DeKast : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +20,44 @@ class DeKast : AppCompatActivity() {
         val button: Button = findViewById(R.id.btn_kastBackHome2)
         button.setOnClickListener {
             startActivity(Intent(this@DeKast, MainActivity::class.java))
+
+            val ListView = findViewById<ListView>(R.id.de_kast_listview)
+
+            ListView.adapter = MyCustomAdapter(this) // this needs to be my custum adapter telling my list what to render
+
+
     }
-}}
+}
+    private class MyCustomAdapter(context: Context): BaseAdapter(){
+
+        private val mContext: Context
+
+        init {
+                mContext = context
+        }
+
+        //verantwoordelijk hoeveel rijen in list
+        override fun getCount(): Int {
+            return 5
+        }
+
+        // kunt het nu negeren
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+        // kunt het nu negeren
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        //verantwoordelijk voor elke rij te renderen
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text="Here is my Row for my LISTVIEW"
+            return textView
+
+        }
+
+    }
+}
+
