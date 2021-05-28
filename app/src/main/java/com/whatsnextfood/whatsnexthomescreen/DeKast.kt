fe5f8e9723cdfc81fe621e5a46de1aec03878913
lasse <r0845495@student.thoasmore.be>
2021-05-28 15:44:59 +0200

    verderwerken 16

---
+++ app/src/main/java/com/whatsnextfood/whatsnexthomescreen/DeKast.kt
@@ -0,0 +1,18 @@
+package com.whatsnextfood.whatsnexthomescreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class DeKast : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_de_kast)
+
+
+        val button: Button = findViewById(R.id.btn_kastBackHome2)
+        button.setOnClickListener {
+            startActivity(Intent(this@DeKast, MainActivity::class.java))
+    }
+}
